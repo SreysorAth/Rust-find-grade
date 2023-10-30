198a928b53c1d69d06e67fe1fdc3514c6c7dfbad
SreysorAth <147776796+SreysorAth@users.noreply.github.com>
2023-10-30 19:49:47 +0700

    Add files via upload

---
+++ find_grade/target/debug/deps/find_grade-60c70ab0d37138b4.d
@@ -0,0 +1,5 @@
+C:\Users\sreysor_pc\Desktop\Ramkhamhaeng University\Computor Science\ComputerScience 2566\Computer science1_66\COS3105\LAB\find_grade\target\debug\deps\find_grade-60c70ab0d37138b4.rmeta: src\main.rs
+
+C:\Users\sreysor_pc\Desktop\Ramkhamhaeng University\Computor Science\ComputerScience 2566\Computer science1_66\COS3105\LAB\find_grade\target\debug\deps\find_grade-60c70ab0d37138b4.d: src\main.rs
+
+src\main.rs:
